54767c5fba7c00c0d51fe442e37c740ae4485b50
Rishabh Tewari <rishabhtewari1@gmail.com>
2020-11-20 21:55:53 -0600

    new file:   SphereMover.cs 	new file:   WallPhysics.cs 	new file:   ../CollisionShit.sln

---
+++ Assets/WallPhysics.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPhysics : MonoBehaviour
+{
+    public GameObject green;
+    public GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        green = GameObject.FindGameObjectWithTag("Green");
+        wall = GameObject.Find("Wall");
+        //if(collision.collider.CompareTag("Green") || collision.collider.CompareTag("Red"))
+        //{
+        Debug.Log(collision.collider.tag + " hit the wall");
+        //}
+        if (collision.collider.CompareTag("Green"))
+        {
+            //Destroy(green.GetComponent<SphereCollider>()); //Stupid way to do this
+            //Chad way to do this
+            Physics.IgnoreCollision(green.GetComponent<SphereCollider>(), wall.GetComponent<BoxCollider>() );
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
